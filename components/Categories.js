759e2b00669860ec7c708c75a011fa8b7f98ad9e
Berkay Uygur <50241715+uygurberkay@users.noreply.github.com>
2023-07-12 13:49:46 +0300

    v.02

---
+++ components/Categories.js
@@ -0,0 +1,23 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <View className="mt-4">
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                className="overflow-visible"
+                contentContainerStyle={{
+                    paddingHorizontal: 15
+                }}
+            >
+                {
+                    
+                }
+            </ScrollView>
+        </View>
+    )
+}
+
+export default Categories
---
+++ screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import { View, Text, TextInput, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import * as Icon from "react-native-feather";
+import {themeColors} from '../theme'
+import Categories from '../components/Categories';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+            <StatusBar barStyle="dark-content"/>
+            {/* Search Bar */}
+            <View className="flex-row items-center space-x-2 px-4 pb-2 ">
+                <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
+                    <Icon.Search height="25" width="25" stroke="gray" />
+                    <TextInput placeholder='Resturants' className="ml-2 flex-1" keyboardType='default' />
+                    <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
+                        <Icon.MapPin height="20" width="20" stroke="gray" />
+                        <Text className="text-gray-600">Konak, İzmir</Text>
+                    </View>
+                </View>
+                <View style={{backgroundColor: themeColors.bgColor(1)}} className="p-3 rounded-full">
+                    <Icon.Sliders height={20} width={20} strokeWidth="2.5" stroke="white" />
+                </View>
+            </View>
+            {/* main */}
+            <ScrollView  
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingBottom: 20
+                }}
+            >
+            {/* categories */}
+            <Categories />
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
