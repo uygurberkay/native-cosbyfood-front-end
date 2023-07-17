f8ef14779bf82c6c3ae6a0b627dc7944df8fd619
Berkay Uygur <50241715+uygurberkay@users.noreply.github.com>
2023-07-17 17:21:54 +0300

    v.14

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import Navigation from './navigation';
-// import { store } from './store'
-// import { Provider } from 'react-redux'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 export default function App() {
   return (
-    // <Provider store={store}>
+    <Provider store={store}>
     <Navigation />
-    // </Provider>
+    </Provider>
   );
 }
 
